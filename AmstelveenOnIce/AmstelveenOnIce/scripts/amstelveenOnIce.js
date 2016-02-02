978108a3d9db0ae45d9a84eb08173ab6bfbd55a2
pousette <hmenting-gresnich@hotmail.com>
2016-02-02 11:36:01 +0100

    snow script

--- AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
+++ AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
@@ -1,1 +1,3 @@
-﻿ document.getElementById("logo").innerHTML = "<h1>Hier komt het logo</h1>";
+﻿document.getElementById("logo").innerHTML = "<h1>Hier komt het logo</h1>";
+document.getElementById("mainnav").innerHTML = "<a href='home.html'>Home</a> " +
+            "<a href='contact.html'>Contact</a>";

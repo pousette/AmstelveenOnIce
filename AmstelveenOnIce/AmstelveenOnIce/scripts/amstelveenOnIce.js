2fbca0e73d3cd9f9bd7a5f5638f5bd81b523a57f
pousette <hmenting-gresnich@hotmail.com>
2016-02-08 11:11:36 +0100

    pagina toegevoegd

--- AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
+++ AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
@@ -1,3 +1,3 @@
 ﻿document.getElementById("logo").innerHTML = "<img src='images/logo.png'/>";
 document.getElementById("mainnav").innerHTML = "<a href='home.html'>Home</a> " +
-           "<a href='inschrijfformulier.html'>inschrijfformulier</a> " + "<a href='contact.html'>Contact</a> ";
+           "<a href='inschrijfformulier.html'>inschrijfformulier</a> "  + "<a href='informatie.html'>Informatie</a> " + "<a href='contact.html'>Contact</a> ";

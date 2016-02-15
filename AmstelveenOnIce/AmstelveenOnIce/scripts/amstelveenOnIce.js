54f5e67a9b198646b68e894d356ed17479183770
Thijs van Hout <thijs.vanhout@co3logie.com>
2016-02-15 18:09:27 +0100

    Schaatsles formulier zichtbaar na button click

--- AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
+++ AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
@@ -2,3 +2,7 @@
 document.getElementById("mainnav").innerHTML = "<a href='home.html'>Home</a> " +
            "<a href='schaatsles.html'>Schaatsles</a> " + "<a href='informatie.html'>Informatie</a> "+ "<a href='tarieven.html'>Tarieven</a> " + "<a href='contact.html'>Contact</a> ";
 document.getElementById("footer").innerHTML = "&copy; 2016 Amstelveen on Ice";
+
+function hideForm() {
+    document.getElementById("zichtbaar").style.display = "block";
+};

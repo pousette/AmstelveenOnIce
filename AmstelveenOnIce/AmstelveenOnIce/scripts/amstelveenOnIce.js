5ceb0c385b8abc3cb82ff1911a072a0568719a8e
pousette <hmenting-gresnich@hotmail.com>
2016-02-15 11:58:17 +0100

    pagina tarieven toegevoegd

--- AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
+++ AmstelveenOnIce/AmstelveenOnIce/scripts/amstelveenOnIce.js
@@ -1,4 +1,4 @@
 ﻿document.getElementById("logo").innerHTML = "<a href='home.html'><img src='images/logo.png'/></a>";
 document.getElementById("mainnav").innerHTML = "<a href='home.html'>Home</a> " +
-           "<a href='schaatsles.html'>Schaatsles</a> " + "<a href='informatie.html'>Informatie</a> " + "<a href='contact.html'>Contact</a> ";
+           "<a href='schaatsles.html'>Schaatsles</a> " + "<a href='informatie.html'>Informatie</a> "+ "<a href='tarieven.html'>Tarieven</a> " + "<a href='contact.html'>Contact</a> ";
 document.getElementById("footer").innerHTML = "&copy; 2016 Amstelveen on Ice";
